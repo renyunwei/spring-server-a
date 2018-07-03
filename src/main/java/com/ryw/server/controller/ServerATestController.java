bdf7af6dcf824060cd43a9d70fafc175a175e730
renyunwei <renyw789@163.com>
2018-07-03 13:53:46 +0800

    接入注册中心以及配置中心

--- src/main/java/com/ryw/server/controller/ServerATestController.java
+++ src/main/java/com/ryw/server/controller/ServerATestController.java
@@ -3,6 +3,7 @@ package com.ryw.server.controller;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.client.serviceregistry.Registration;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -16,9 +17,12 @@ public class ServerATestController {
 	@Autowired
 	private Registration registration;
 	
+	@Value("${server.port}")
+	private String serverPort;
+	
 	@RequestMapping(value = "hello-test", method = RequestMethod.GET)
 	public String helloWorldA() {
-		logger.info("hello world A server");
+		logger.info("hello world A server,this service's port is {}",serverPort);
 		logger.info("/hello-test, host:" + registration.getHost() + ", service_id:" + registration.getServiceId());
 		return "hello world A server";
 	}
